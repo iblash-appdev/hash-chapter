0c27f67160b6ec11551a27989cf8c975f98949d4
Milton Lash <iblash@live.com>
2023-02-28 19:48:53 +0000

    Completed hash_person

--- hash_person.rb
+++ hash_person.rb
@@ -2,3 +2,10 @@
 # Make sure the value of the :age key is an Integer
 
 p "Enter a name, age, and occupation separated by spaces:"
+person1 = gets.chomp.split
+
+#p person1.at(0) + person1.at(1) + person1.at(2)
+
+person1 = {:name => person1.at(0),:age=> person1.at(1).to_i,:occupation => person1.at(2)}
+
+p person1
